952d5ebb8929e857117b82ef2dda704fa72125c7
minimallinux <anthonymcgowan1@gmail.com>
2017-10-20 10:38:24 +0100

    First Commit

---
+++ js/routes.js
@@ -0,0 +1,20 @@
+﻿var routes = [
+  // Index page
+  {
+    path: '/',
+    url: './index.html',
+  },
+  // Pages
+  {
+    path: '/about/',
+    url: './pages/about.html',
+  },
+  {
+      path: '/services/',
+      url: './pages/services.html',
+  },
+  {
+      path: '/form/',
+      url: './pages/form.html',
+  }
+];
